c1188e9484de293aa064b3c291f917ccb7481e86
khelixx <guillaume.verdugo@gmail.com>
2016-01-24 14:39:24 +0100

    labyrinthe ongoing and add entities

---
+++ GameFramework/src/game_entities/sonics.java
@@ -0,0 +1,38 @@
+package game_entities;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+import gameframework.base.Drawable;
+import gameframework.base.DrawableImage;
+import gameframework.base.Overlappable;
+import gameframework.game.GameEntity;
+import gameframework.game.GameMovable;
+
+public class sonics extends GameMovable implements Drawable, GameEntity, Overlappable {
+	public static final int RENDERING_SIZE = 40;
+	protected static DrawableImage image = null;
+	Canvas canvas;
+	protected Point position;
+	
+	public sonics(Canvas canvas){
+			this.canvas = canvas;
+			image = new DrawableImage("assets/images/ball.gif", canvas);
+	}
+
+
+	public void draw(Graphics g) {
+		 g.drawImage(image.getImage(), (int) getPosition().getX(),
+	                (int) getPosition().getY(), RENDERING_SIZE, RENDERING_SIZE, null);
+
+	}
+
+	public Rectangle getBoundingBox() {
+		 return new Rectangle(0, 0, RENDERING_SIZE, RENDERING_SIZE);
+	}
+
+	@Override
+	public void oneStepMoveAddedBehavior() {
+	}
+}
